0bf8336ec4022f5945c4cdb9a023ec347ce32bc8
mark <markwani70@googlemail.com>
2016-11-29 14:55:34 +0000

    Add project boilerplate

---
+++ js/app.js
@@ -0,0 +1,25 @@
+angular.module('AppS1', ['ngRoute', 'RouteControllers']);
+ 
+angular.module('AppS1').config(function($routeProvider) {
+ 
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/events', {
+        templateUrl: 'templates/events.html',
+        controller: 'EventsController'
+    })
+    .when('/songs', {
+        templateUrl: 'templates/songs.html',
+        controller: 'SongsController'
+    })
+    .when('/gallery', {
+        templateUrl: 'templates/gallery.html',
+        controller: 'GalleryController' 
+    })
+    .when('/bookings', {
+        templateUrl: 'templates/bookings.html',
+        controller: 'BookingsController'      
+    });
+});

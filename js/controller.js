bbe2523907a1aa353c8d53202437d4aeb8a244e8
mark <markwani70@googlemail.com>
2017-01-13 16:41:33 +0000

    Add project boilerplate

--- js/controller.js
+++ js/controller.js
@@ -7,7 +7,58 @@ angular.module('RouteControllers', [])
     })
     .controller('SongsController', function($scope) {
         $scope.title = "songs"
+        $scope.singles = [
+            {
+             name: "1. Clarksville (New)",
+             cover: "/assets/images/album1.jpg",
+             track: "/assets/audio/Clarksville.mp3",
+            },
+
+            {
+             name: "2. Day beliver (New)",
+             cover: "/assets/images/album2.jpg",
+             track: "/assets/audio/DaydreamBeliever.mp3",
+            },
+              
+            {
+             name: "3. I am a beliver",
+             cover: "/assets/images/album3.jpg",
+             track: "/assets/audio/ImABeliever.mp3",
+            },
+
+            {
+             name: "4. Stepping stone",
+             cover: "/assets/images/album4.jpg",
+             track: "/assets/audio/SteppingStone.mp3",
+            },
+
+            {
+             name: "5. Headquarters",
+             cover: "/assets/images/album5.jpg",
+             track: "/assets/audio/tailtoddle_lo.mp3",
+            },
+
+            {
+             name: "6. Good Times",
+             cover: "/assets/images/album6.jpg",
+             track: "/assets/audio/SteppingStone.mp3",
+            },
+
+            {
+             name: "7. Missing Links",
+             cover: "/assets/images/album7.jpg",
+             track: "/assets/audio/DaydreamBeliever.mp3",
+            },
+
+            {
+             name: "8. Instant Replay",
+             cover: "/assets/images/album8.jpg",
+             track: "/assets/audio/Clarksville.mp3",
+            }
+        ];
+
     })
+
     .controller('GalleryController', function($scope) {
         $scope.title = "gallery"
     })
